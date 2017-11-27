f28e80443c0766bb57599028a3518ee137e6274b
Ollin Langle <ollin.langle@ciencias.unam.mx>
2017-11-26 21:41:50 -0600

    deleted exec

--- execute.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./Scraping/execute.sh
-./DB/execute.sh
-julia Networks/src/party_color.jl
-julia Networks/src/communities_langle.jl
-julia Networks/src/coalitions.jl
-julia Networks/src/in_and_out.jl
-julia Networks/src/cc_plot.jl
