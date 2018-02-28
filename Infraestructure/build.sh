3dbe584d5c3c57c5760962a048019708b5edb039
Ollin Langle <ollin.langle@ciencias.unam.mx>
2018-02-28 10:05:22 -0600

    add db docker to build.sh and fix path to nodes and edges in db docker

--- Infraestructure/build.sh
+++ Infraestructure/build.sh
@@ -1,1 +1,2 @@
-docker build -f Dockerfile --tag ollin18/scraping:0.1 --tag ollin18/scraping:latest .
+docker build -f Dockerfile.scrap --tag ollin18/scraping:0.1 --tag ollin18/scraping:latest .
+docker build -f Dockerfile.db --tag ollin18/neo_senate:0.1 --tag ollin18/neo_senate:latest .
