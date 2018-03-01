508ac219df3f3b40bd0d24fa4759ed069d737e8f
Ollin Langle <ollin.langle@ciencias.unam.mx>
2018-02-28 21:57:17 -0600

    delete file in mislocation

--- Infraestructure/build.sh
+++
@@ -1,2 +0,0 @@
-docker build -f Dockerfile.scrap --tag ollin18/scraping:0.1 --tag ollin18/scraping:latest .
-docker build -f Dockerfile.db --tag ollin18/neo_senate:0.1 --tag ollin18/neo_senate:latest .
