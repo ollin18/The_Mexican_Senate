d6a95627b2b988bfa01b0b0f0bc6318a3eb31b10
Ollin Langle <ollin.langle@ciencias.unam.mx>
2018-03-05 22:46:02 -0600

    mount volumes at run to create db after build

--- DB/execute.sh
+++ DB/execute.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#./create_csv.sh
+./create_csv.sh
 #./create_db.sh
 
 #docker build --tag senate_neo:0.1 --tag senate_neo:latest .
 
-docker run \
-    --publish=7474:7474 --publish=7473:7473 \
+docker run -v $(pwd)/nodes:/nodes -v $(pwd)/edges:/edges \
+    --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/data:/data \
     --volume=$HOME/neo4j/logs:/logs \
     --env=NEO4J_AUTH=none \
